0b047822fad52b2ab80ed91fbc4a635d23831706
waltim <waltimlmm@gmail.com>
2024-09-24 17:24:17 -0300

    classe user dto

---
+++ src/main/java/br/com/waltim/api/domain/dto/UserDTO.java
@@ -0,0 +1,18 @@
+package br.com.waltim.api.domain.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserDTO {
+    private Long id;
+    private String name;
+    private String email;
+    private String password;
+}
