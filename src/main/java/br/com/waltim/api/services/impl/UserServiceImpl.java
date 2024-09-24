5303d18b11ef3249f8c94c83afb08181517625a3
waltim <waltimlmm@gmail.com>
2024-09-24 17:17:11 -0300

    tratamento de exceção personalizado

--- src/main/java/br/com/waltim/api/services/impl/UserServiceImpl.java
+++ src/main/java/br/com/waltim/api/services/impl/UserServiceImpl.java
@@ -3,6 +3,7 @@ package br.com.waltim.api.services.impl;
 import br.com.waltim.api.domain.Users;
 import br.com.waltim.api.repositories.UserRepository;
 import br.com.waltim.api.services.UserService;
+import br.com.waltim.api.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -17,6 +18,6 @@ public class UserServiceImpl implements UserService {
     @Override
     public Users findById(Long id) {
         Optional<Users> user = repository.findById(id);
-        return user.orElse(null);
+        return user.orElseThrow(() -> new ObjectNotFoundException("Usuário não encontrado"));
     }
 }
