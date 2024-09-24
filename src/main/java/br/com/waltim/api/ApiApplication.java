496295e4722e958c70058309ece4fc8ca4279daf
waltim <waltimlmm@gmail.com>
2024-09-24 16:10:00 -0300

    entity user

--- src/main/java/br/com/waltim/api/ApiApplication.java
+++ src/main/java/br/com/waltim/api/ApiApplication.java
@@ -1,5 +1,6 @@
 package br.com.waltim.api;
 
+import br.com.waltim.api.domain.User;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +9,10 @@ public class ApiApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ApiApplication.class, args);
+
+        User user = new User(1L, "Walter", "waltim@foton.la", "1234");
+
+        System.out.println(user.toString());
     }
 
 }
---
+++ src/main/java/br/com/waltim/api/domain/User.java
@@ -0,0 +1,19 @@
+package br.com.waltim.api.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    @Column(unique = true)
+    private String email;
+    private String password;
+}
