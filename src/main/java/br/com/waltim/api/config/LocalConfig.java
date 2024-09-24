ed67bc8ea18baa560a1d53df09fd09698b29cd51
waltim <waltimlmm@gmail.com>
2024-09-24 16:55:01 -0300

    adicionando perfil de testes

---
+++ src/main/java/br/com/waltim/api/config/LocalConfig.java
@@ -0,0 +1,31 @@
+package br.com.waltim.api.config;
+
+import br.com.waltim.api.domain.Users;
+import br.com.waltim.api.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import java.util.List;
+
+@Configuration
+@Profile("local")
+public class LocalConfig implements CommandLineRunner {
+
+    @Autowired
+    private UserRepository repository;
+
+    @Override
+    public void run(String... args) throws Exception {
+        startDB();
+    }
+
+    private void startDB() {
+        Users u1 = new Users(null, "Carol", "carolzinha@teste.com", "123");
+        Users u2 = new Users(null, "Elisa", "xuxu@teste.com", "123");
+
+        repository.saveAll(List.of(u1, u2));
+    }
+}
