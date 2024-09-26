1fbc57a6146eb6caa0e142f30c59f91934d1992e
waltim <waltimlmm@gmail.com>
2024-09-26 20:22:17 -0300

    removendo warnings ao subir aplicação e removendo anotação desnecessaria.

--- src/main/java/br/com/waltim/api/repositories/UserRepository.java
+++ src/main/java/br/com/waltim/api/repositories/UserRepository.java
@@ -2,11 +2,9 @@ package br.com.waltim.api.repositories;
 
 import br.com.waltim.api.domain.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
-@Repository
 public interface UserRepository extends JpaRepository<Users, Long> {
     Optional<Users> findByEmail(String email);
 }
