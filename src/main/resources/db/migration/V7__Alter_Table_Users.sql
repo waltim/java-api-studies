0184ae30a734991a00a52c9b018daca03f01cf7d
waltim <waltimlmm@gmail.com>
2024-10-08 15:32:47 -0300

    Create new tables and insert initial data
    Added new `user_permission` and `permission` tables, including necessary constraints and initial data insertions. Updated the `users` table to include additional columns and populated them with data accordingly.

---
+++ src/main/resources/db/migration/V7__Alter_Table_Users.sql
@@ -0,0 +1,25 @@
+﻿-- Flyway migration script to alter the 'users' table
+
+-- Adding the missing columns to the 'users' table
+ALTER TABLE users
+    ADD COLUMN user_name VARCHAR(255) DEFAULT NULL,
+ADD COLUMN full_name VARCHAR(255) DEFAULT NULL,
+ADD COLUMN account_non_expired BIT(1) DEFAULT NULL,
+ADD COLUMN account_non_locked BIT(1) DEFAULT NULL,
+ADD COLUMN credentials_non_expired BIT(1) DEFAULT NULL,
+ADD COLUMN enabled BIT(1) DEFAULT NULL;
+
+-- Adding unique constraint on 'user_name'
+ALTER TABLE users
+    ADD CONSTRAINT uk_user_name UNIQUE (user_name);
+
+
+-- Atualizando registros já existentes na tabela `users` para preencher as novas colunas
+UPDATE users
+SET user_name = email,  -- Usando o campo `email` como `user_name`
+    full_name = name,   -- Usando o campo `name` como `full_name`
+    password = "19bbf735b27066f2f145e602624e1b24a3fbc54cd5dfd3143fc5feea6bdee9e139ca7332d4806b9f",
+    account_non_expired = 1,
+    account_non_locked = 1,
+    credentials_non_expired = 1,
+    enabled = 1;
---
+++ src/main/resources/db/migration/V8__Insert_Data_In_Users.sql
@@ -0,0 +1,6 @@
+﻿-- Inserção com o novo formato de tabela
+INSERT INTO users (id, city, complement, country, email, name, number, password, state, street, user_name, full_name,
+                   account_non_expired, account_non_locked, credentials_non_expired, enabled)
+VALUES
+    (3, 'New York', 'Apt 5C', 'USA', 'joao@teste.com', 'João', '456', '19bbf735b27066f2f145e602624e1b24a3fbc54cd5dfd3143fc5feea6bdee9e139ca7332d4806b9f', 'NY', '5th Avenue', 'joao@teste.com', 'João da Silva', 1, 1, 1, 1),
+    (4, 'Chicago', 'Apt 2A', 'USA', 'maria@teste.com', 'Maria', '789', '75ec349c1b0ef4ee7b249d0b83ae4861853f3aa77bce8c4b15f28cd43c6424ab4f29df431831bb0d', 'IL', 'Lake Shore Drive', 'maria@teste.com', 'Maria Souza', 1, 1, 1, 1);
