9674fb69589daf61de00d31e887c8e3d781c29a4
waltim <waltimlmm@gmail.com>
2024-10-10 16:49:34 -0300

    comentando código apenas para gerar um hash de test na class ApiApplication.

--- src/main/java/br/com/waltim/api/ApiApplication.java
+++ src/main/java/br/com/waltim/api/ApiApplication.java
@@ -14,30 +14,27 @@ import java.util.Map;
 @SpringBootApplication
 public class ApiApplication {
 
-    @Value("${security.jwt.token.secret-key}")
-    private String secretKey; // Remover o static aqui
-
-    private PasswordEncoder passwordEncoder; // Mover a declaração para a classe
+//    @Value("${security.jwt.token.secret-key}")
+//    private String secretKey; // Remover o static aqui
+//
+//    private PasswordEncoder passwordEncoder; // Mover a declaração para a classe
 
     public static void main(String[] args) {
         SpringApplication.run(ApiApplication.class, args);
     }
 
-    @PostConstruct
-    public void init() {
-        // Cria um codificador PBKDF2
-        Pbkdf2PasswordEncoder pbkdf2PasswordEncoder =
-                new Pbkdf2PasswordEncoder(secretKey, 16, 10000, Pbkdf2PasswordEncoder.SecretKeyFactoryAlgorithm.PBKDF2WithHmacSHA256);
-
-        // Cria um mapa para encoders
-        Map<String, PasswordEncoder> encoders = new HashMap<>();
-        encoders.put("pbkdf2", pbkdf2PasswordEncoder);
-
-        // Cria o DelegatingPasswordEncoder com "pbkdf2" como o método de codificação padrão
-        passwordEncoder = new DelegatingPasswordEncoder("pbkdf2", encoders);
-
-        // Exemplo de uso do passwordEncoder para codificar uma senha
-        String result = passwordEncoder.encode("admin234");
-        System.out.println("My hash " + result);
-    }
+//    @PostConstruct
+//    public void init() {
+//        // Cria um codificador PBKDF2
+//        Pbkdf2PasswordEncoder pbkdf2PasswordEncoder =
+//                new Pbkdf2PasswordEncoder(secretKey, 16, 10000, Pbkdf2PasswordEncoder.SecretKeyFactoryAlgorithm.PBKDF2WithHmacSHA256);
+//
+//        Map<String, PasswordEncoder> encoders = new HashMap<>();
+//        encoders.put("pbkdf2", pbkdf2PasswordEncoder);
+//
+//        passwordEncoder = new DelegatingPasswordEncoder("pbkdf2", encoders);
+//
+//        String result = passwordEncoder.encode("admin234");
+//        System.out.println("My hash " + result);
+//    }
 }
